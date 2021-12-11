e9ce992daa9f36f4e7d497d1eefe679794e694d6
Hitesh Mandav <mandavhitesh@gmail.com>
2021-12-11 17:35:03 +0530

    To keep server alive

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const keepAwake = require("./keepAwake");
 const app = express();
-app.use(express.static(__dirname + '/dist/final-portfolio-project'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/final-portfolio-project/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + "/dist/final-portfolio-project"));
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname + "/dist/final-portfolio-project/index.html")
+  );
+});
+app.listen(process.env.PORT || 8080, () => {
+  const APPURL = "https://hitesh-mandav.herokuapp.com/";
+  keepAwake(APPURL);
+});
